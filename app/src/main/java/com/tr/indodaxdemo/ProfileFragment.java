fcf09299ed26fa6044bb600ce08715f575e53be6
aditiyapw <aditiyaputra999950@gmail.com>
2021-04-23 16:52:37 +0700

    (ditambah fragment : home, wallet, profile) udah ditambah fragment buat detail akun/profil

--- app/src/main/java/com/tr/indodaxdemo/ProfileFragment.java
+++ app/src/main/java/com/tr/indodaxdemo/ProfileFragment.java
@@ -1,19 +1,42 @@
 package com.tr.indodaxdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
-    @Nullable
+    TextView mProfile;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile,container,false);
+
+        mProfile = v.findViewById(R.id.profile);
+
+        mProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProfileDetailFragment profileDetailFragment = new ProfileDetailFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,profileDetailFragment);
+                transaction.commit();
+            }
+        });
+
+        return v;
     }
+
 }
+
